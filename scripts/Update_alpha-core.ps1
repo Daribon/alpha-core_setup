b61e12c54d1a16771dae1c1b8134213f575e372d
Daribon <dartarifon@hotmail.com>
2023-04-14 00:07:33 +0200

    Fix python path

--- scripts/Update_alpha-core.ps1
+++ scripts/Update_alpha-core.ps1
@@ -1,9 +1,8 @@
 #Requires -Version 7.0
-cd alpha-core-master
+Set-Location alpha-core-master
 if ($IsWindows) {
-    $env:Path += ';C:\Program Files\MariaDB 10.9\bin'
-    $env:Path += ';C:\Users\IPSUM\AppData\Local\Programs\Python\Python311'
-    $env:Path += ';C:\Users\IPSUM\AppData\Local\Programs\Python\Python311\Scripts'
+    $env:py = (';' + $env:LOCALAPPDATA + '\Programs\Python\Python311' + ';' + $env:LOCALAPPDATA + '\Programs\Python\Python311\Scripts')
+    $env:Path += ($env:py + ';' + 'C:\Program Files\MariaDB 10.9\bin')
 }
 pip3 install -r requirements.txt
 $username = ($v = Read-Host "Enter your database username [root]") ? $v : 'root'
@@ -12,7 +11,7 @@ $port = ($v = Read-Host "Enter your database port [3306]") ? $v : '3306'
 $realm = ($v = Read-Host "Enter your realm database [alpha_realm]") ? $v : 'alpha_realm'
 $dbc = ($v = Read-Host "Enter your dbc database [alpha_dbc]") ? $v : 'alpha_dbc'
 $world = ($v = Read-Host "Enter your world database [alpha_world]") ? $v : 'alpha_world'
-start mariadbd.exe --console
+Start-Process mariadbd.exe --console
 mariadb --user=$username --password=$password --port=$port $realm -e "source etc\databases\realm\updates\updates.sql"
 mariadb --user=$username --password=$password --port=$port $dbc -e "source etc\databases\dbc\updates\updates.sql"
 mariadb --user=$username --password=$password --port=$port $world -e "source etc\databases\world\updates\updates.sql"
@@ -25,4 +24,5 @@ Rename-Item -Path "etc/config/config.yml.dist" -NewName "config.yml"
 (Get-Content etc/config/config.yml) -Replace "realm_db: alpha_realm", "realm_db: $realm" | Set-Content etc/config/config.yml
 (Get-Content etc/config/config.yml) -Replace "dbc_db: alpha_dbc", "dbc_db: $dbc" | Set-Content etc/config/config.yml
 (Get-Content etc/config/config.yml) -Replace "world_db: alpha_world", "world_db: $world" | Set-Content etc/config/config.yml
+(Get-Content etc/config/config.yml) -Replace 'auto_create_gm_accounts: False', 'auto_create_gm_accounts: True' | Set-Content etc/config/config.yml
 py main.py
